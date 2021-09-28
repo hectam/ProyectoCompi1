167cb00d14ccbcd465725da3ada20325876e0dc3
hectam <41806726+hectam@users.noreply.github.com>
2021-09-27 19:06:18 -0500

    asddas

--- DateTimeCompiler.Lexer/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DateTimeCompiler.Lexer
-{
-    public static class Result
-    {
-        public static Result<T> Empty<T>(Input reminder) => new Result<T>(reminder);
-
-        public static Result<T> Value<T>(T value, Input reminder) => new Result<T>(value, reminder);
-    }
-}
